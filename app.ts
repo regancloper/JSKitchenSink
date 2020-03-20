fa367205a10e517e2a39ef6591aa0d58fd4e1ed6
Regan Loper <reganloper@Regans-MBP.attlocal.net>
2020-03-20 11:41:28 -0500

    added typescript

--- app.ts
+++ app.ts
@@ -1,19 +1,31 @@
 // declares a variable called name and sets it to author's name
-var myName = "Regan Loper";
+let myName: string = "Regan Loper";
+
 // declares a constant for the number of U.S. states
-var NUM_STATES = 50;
+const NUM_STATES: number = 50;
+
 // computes the sum of 5 and 4
-var sum = 5 + 4;
+let sum: number = 5 + 4;
+
 sayHello();
+
 checkAge("Charles", 21);
 checkAge("Abby", 27);
 checkAge("James", 18);
 checkAge("John", 17);
-var bestveggies = ["okra", "broccoli", "onion", "carrot", "pickles"];
-for (var i = 0; i < bestveggies.length; i++) {
+
+let bestveggies: string[] = ["okra", "broccoli", "onion", "carrot", "pickles"];
+
+for (let i = 0; i < bestveggies.length; i++) {
     console.log(bestveggies[i]);
 }
-var people = [
+
+interface Person {
+    name: string;
+    age: number;
+}
+
+let people: Array<Person> = [
     {
         name: "Jessie",
         age: 25
@@ -35,24 +47,28 @@ var people = [
         age: 26
     }
 ];
-for (var i = 0; i < people.length; i++) {
+
+for (let i = 0; i < people.length; i++) {
     checkAge(people[i].name, people[i].age);
 }
-var strlength = getLength("Hello World");
+
+let strlength: number = getLength("Hello World");
 if (strlength % 2 == 0) {
     console.log("The world is nice and even!");
-}
-else {
+} else {
     console.log("The world is an odd place!");
 }
+
 function sayHello() {
     alert("Hello World!");
 }
-function checkAge(name, age) {
+
+function checkAge(name: string, age: number) {
     if (age < 21) {
         alert("Sorry " + name + ", you aren't old enough to view this page!");
     }
 }
-function getLength(word) {
+
+function getLength(word: string) {
     return word.length;
 }
